fe877cc12a0e8254456bd634f15d5fd896d5496b
William Viktorsson <wilvik05@edu.umea.se>
2024-09-06 10:15:45 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Presentation, Slide, Transition } from '@animotion/core'
+
+
+</script>
+
+<Presentation options={{ history: true, transition: 'slide', controls: true, progress: true }} class="flex h-full items-center">
+	<Slide animate>
+		<ol>
+			<Transition><li>example</li></Transition>
+			<Transition><li>exampler</li></Transition>
+			<Transition><li>examplest</li></Transition>
+			<Transition><li>examplestest</li></Transition>
+		</ol>
+	</Slide>
+
+	<Slide animate>
+		<ol>
+			<li class="fragment">working example</li>
+			<li class="fragment">working exampler</li>
+			<li class="fragment">working examplest</li>
+			<li class="fragment">working examplestest</li>
+		</ol>
+	</Slide>
+</Presentation>
